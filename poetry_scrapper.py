ce97aff98e6887950047e4c8b3ff34f98bc1396c
Preep <rjirfvfirf@gmail.com>
2020-11-02 23:23:21 +1000

    Make stdout more consistent

--- poetry_scrapper.py
+++ poetry_scrapper.py
@@ -27,23 +27,26 @@ def load_bookmark():
     }
     return page_dict
 
+
 def page_dict_to_url(page_dict):
     url = f"https://stihi.ru/{page_dict['year']}/{page_dict['month']:02}/{page_dict['day']:02}/{page_dict['number']}"
     return url
 
+
 def flip_page(page_dict, count=1):
     if page_dict['number'] <= count:
-        this_date = datetime.date(page_dict['year'], page_dict['month'], page_dict['day'])
+        this_date = datetime.date(
+            page_dict['year'], page_dict['month'], page_dict['day'])
         new_date = this_date - datetime.timedelta(1)
-        
+
         page_dict['year'] = new_date.year
         page_dict['month'] = new_date.month
         page_dict['day'] = new_date.day
         page_dict['number'] = 5000
-    
+
     else:
         page_dict['number'] -= count
-    
+
     return page_dict
 
 
@@ -61,22 +64,22 @@ def main():
             response = requests.get(url)
             status_code = response.status_code
         except:
-            print('Connection error!')
+            print(f'{url} | Connection error!')
             status_code = 0
 
         if status_code != 200:
             bad_status_in_a_row_count += 1
             print('Status code not 200. Initiating evasive maneuver')
             current_page = flip_page(current_page, count=1000)
-       
+
         else:
             bad_status_in_a_row_count = 0
-            
+
             soup = BeautifulSoup(response.text, 'html.parser')
             poem = soup.find('div', {'class': 'text'})
             if poem is None:
                 current_page = flip_page(current_page, count=100)
-                print(f'Skipping {url}')
+                print(f'{url} | skipped')
                 continue
             poem = poem.text.strip()
             poem = poem.replace('\n', '|').replace('\xa0', '')
@@ -86,8 +89,8 @@ def main():
                 writer.writerow([poem])
 
             current_page = flip_page(current_page)
-            print(url)
-            
+            print(f'{url} | ok')
+
     else:
         print('Finished or stuck, go check something')
         return 0
@@ -106,11 +109,11 @@ if __name__ == '__main__':
         with open('bookmark.txt', 'r') as f:
             bookmark_after = f.read()
 
-        if bookmark_before==bookmark_after and p.is_alive():
+        if bookmark_before == bookmark_after and p.is_alive():
             p.kill()
             p.join(5)
             p = multiprocessing.Process(target=main)
             p.start()
             print('RESTARTING PROCESS')
         elif p.is_alive():
-            bookmark_before=bookmark_after
+            bookmark_before = bookmark_after
